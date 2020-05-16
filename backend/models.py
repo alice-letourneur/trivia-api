d740f1728e9006f9d95d7c4f09a25fa4e71095a1
“Alice <aliceletourneur@icloud.com>
2020-05-16 17:43:34 +0100

    connect to my local db

--- backend/models.py
+++ backend/models.py
@@ -3,8 +3,9 @@ from sqlalchemy import Column, String, Integer, create_engine
 from flask_sqlalchemy import SQLAlchemy
 import json
 
+database_username = "aliceletourneur"
 database_name = "trivia"
-database_path = "postgres://{}/{}".format('localhost:5432', database_name)
+database_path = "postgres://{}@{}/{}".format(database_username, 'localhost:5432', database_name)
 
 db = SQLAlchemy()
 
